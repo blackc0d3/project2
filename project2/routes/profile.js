b7243f58a80fee11abde718e66205916ba612803
Paty Gilbon <anapatygilbon@gmail.com>
2017-07-19 14:33:37 +0200

    mier 14:30

--- project2/routes/profile.js
+++ project2/routes/profile.js
@@ -14,12 +14,6 @@ const Picture           = require('../models/pictures');
 // Upload pictures
 const upload = multer({ dest: './public/uploads/' });
 
-/* GET home page. */
-router.get('/', (req, res) => {
-    //res.render('profile/profile');
-    res.render('profile');
-});
-
 router.get('/edit', (req, res) => {
     res.render('profile/edit', {
         campusTypes,
@@ -97,7 +91,7 @@ router.post('/edit', (req, res, next) => {
         department: req.body.department,
         skills: req.body.skills
     };
-    
+
     User.findByIdAndUpdate(userId, updates, (err, user) => {
         if (err) {
             return res.render('profile/edit', {
@@ -108,35 +102,32 @@ router.post('/edit', (req, res, next) => {
         if (!user) {
             return next(new Error("404"));
         }
-        return res.redirect('/profile');
+        return res.redirect('/profile/myprofile');
     });
 });
 
 router.post('/', upload.single('photo'), function(req, res){
-    const userId = req.user._id;
-    console.log("este es el ID",userId);
+    const filename=req.file.filename;
+    const extension=req.file.mimetype.split('/')[1];
+
     let pic;
     pic = new Picture({
-//    name: req.body.name,
-    pic_path: `/uploads/${req.file.filename}`,
+    name: req.body.name,
+    pic_path: `/uploads/${filename}.${extension}`,
     pic_name: req.file.originalname
   });
-    
-    console.log("IMG NAME",req.file.filename);
-    
-    const update = {
-        img: req.file.filename
-    };
-    console.log(update);
-    
 
   pic.save((err) => {
-
-      res.redirect('/information');
+    res.redirect('/information');
   });
 });
 
-
+router.get('/myprofile', (req, res) => {
+    var user = req.user;
+    Picture.find((err, pictures) => {
+      res.render('profile/myprofile' , {user, pictures} );
+    });
+});
 
 
 module.exports = router;
